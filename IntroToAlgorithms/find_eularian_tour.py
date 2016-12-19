4bd1b80e42b62b6da56e1d7673b708c4d3696ee6
alecland <alexisdev@outlook.com>
2016-12-19 17:56:17 +0100

    Update find_eularian_tour.py
    Add a test5
    Get the solution from a stack overflow post, after getting close...

--- IntroToAlgorithms/find_eularian_tour.py
+++ IntroToAlgorithms/find_eularian_tour.py
@@ -12,35 +12,29 @@
 def find_eulerian_tour(graph):
     # your code here
     res = []
-    res.append(graph[0][0])
-    res.append(graph[0][1])
-    del graph[0]
-    length = len(graph)
-    while (length > 0):
-        for i in range(length):
-            # print res[-1];
-            if graph[i][0] == res[-1]:
-                res.append(graph[i][1])
-                del graph[i]
-                break
-            if graph[i][1] == res[-1]:
-                res.append(graph[i][0])
-                del graph[i]
-                break
-        if length == len(graph):
-            res = []
-            break
-        else:
-            length = len(graph)
-            
+    find_tour(graph, res, graph[0][0])
+    
     return res
-
+    
+def find_tour(graph, tour, curNode):
+  # print tour
+  for (a,b) in graph:
+    if a == curNode:
+        graph.remove((a,b))
+        find_tour(graph, tour, b)
+    elif b == curNode:
+        graph.remove((a,b))
+        find_tour(graph, tour, a)
+  # print curNode
+  tour.insert(0, curNode)
 
 input1 = [(1, 2), (2, 3), (3, 1)]
 input2 = [(0, 1), (1, 5), (1, 7), (4, 5),(4, 8), (1, 6), (3, 7), (5, 9),(2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 input3 = [(1, 13), (1, 6), (6, 11), (3, 13),(8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9),(1, 12), (4, 12), (5, 14), (0, 1),(2, 3), (4, 11),(6, 9),(7, 14),(10, 13)]
 input4 = [(8, 16), (8, 18), (16, 17), (18, 19),(3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14),(1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15),(6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
+input5 = [(2, 6), (4, 2), (5, 4), (6, 5), (6, 8), (7, 9), (8, 7), (9, 6)]
 print (find_eulerian_tour(input1))
 print (find_eulerian_tour(input2))
 print (find_eulerian_tour(input3))
 print (find_eulerian_tour(input4))
+print (find_eulerian_tour(input5))
